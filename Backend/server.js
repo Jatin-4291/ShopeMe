1d44f378a3e6b9398309ff673da6494fc095c27c
Jatin-4291 <jatin.dua2003@gmail.com>
2025-09-10 15:58:52 +0530

    updated cors 2

--- Backend/server.js
+++ Backend/server.js
@@ -5,11 +5,28 @@ import passport from "passport";
 import { Strategy } from "passport-google-oauth2";
 import User from "./Models/userModels.js";
 const oauth2Stratergy = Strategy;
+import cors from "cors";
 import dotenv from "dotenv";
 
 dotenv.config({ path: "./.env" });
-
 const frontendURL = process.env.FRONTEND_URL;
+const allowedOrigins = [
+  "http://localhost:5173", // dev
+  process.env.FRONTEND_URL, // production
+];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("CORS not allowed"));
+      }
+    },
+    methods: "GET,POST,PUT,PATCH,DELETE",
+    credentials: true,
+  })
+);
 process.on("uncaughtException", (err) => {
   console.log(err.name, err.message);
   console.log("Unhandled Exception!");
